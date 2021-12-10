7f74500e3f722822dbabd06ca62b4121fc81be6f
anatawa12 <anatawa12@icloud.com>
2021-12-11 00:15:08 +0900

    Separate fml in forge dependent part and launchwrapper part

---
+++ ForgeMod/src/forge1122/java/zone/rong/nukejndilookupfromlog4j/supports/Support1122.java
@@ -0,0 +1,33 @@
+package zone.rong.nukejndilookupfromlog4j.supports;
+
+import com.google.common.eventbus.EventBus;
+import net.minecraftforge.fml.common.DummyModContainer;
+import net.minecraftforge.fml.common.LoadController;
+import net.minecraftforge.fml.common.ModMetadata;
+import net.minecraftforge.fml.relauncher.FMLInjectionData;
+
+@SuppressWarnings("unused") // used by reflection
+public class Support1122 {
+    public static void runSupport() {
+        FMLInjectionData.containers.add(Container.class.getName());
+    }
+
+    public static class Container extends DummyModContainer {
+        public Container() {
+            super(new ModMetadata());
+            ModMetadata meta = this.getMetadata();
+            meta.modId = "nukejndilookupfromlog4j";
+            meta.name = "NukeJndiLookupFromLog4j";
+            meta.description = "Prevents a major vulnerability introduced by log4j from being abused.";
+            meta.version = "1.0.0";
+            meta.authorList.add("Rongmario");
+            meta.credits = "https://github.com/apache/logging-log4j2/pull/608";
+        }
+
+        @Override
+        public boolean registerBus(EventBus bus, LoadController controller) {
+            bus.register(this);
+            return true;
+        }
+    }
+}
