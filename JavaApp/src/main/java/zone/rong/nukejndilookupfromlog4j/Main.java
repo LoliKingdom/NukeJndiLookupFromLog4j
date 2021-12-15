51cbee10545dbe889230090214363855baf2e52d
Zomabies <zomabies@users.noreply.github.com>
2021-12-15 09:06:52 +0800

    make Java Application compatible with other operating system

--- JavaApp/src/main/java/zone/rong/nukejndilookupfromlog4j/Main.java
+++ JavaApp/src/main/java/zone/rong/nukejndilookupfromlog4j/Main.java
@@ -2,12 +2,19 @@ package zone.rong.nukejndilookupfromlog4j;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
+
+    public static final String JNDI_LOOKUP_PATTERN = "glob:**JndiLookup.class";
 
     public static void main(String[] args) {
         JFileChooser chooser = new JFileChooser();
@@ -18,26 +25,37 @@ public class Main {
             File file = chooser.getSelectedFile();
             if (file != null) {
                 try {
-                    Process process = Runtime.getRuntime().exec("zip -d " + file.getAbsolutePath() + " JndiLookup.class");
-                    BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                    String string;
-                    boolean successful = true;
-                    while ((string = in.readLine()) != null) {
-                        if (string.startsWith("I/O error")) {
-                            JOptionPane.showMessageDialog(null, "Permission denied while trying to modify " + file.getName(), "Error", JOptionPane.ERROR_MESSAGE);
-                            successful = false;
-                        } else if (string.startsWith("zip error: Nothing to do!")) {
-                            JOptionPane.showMessageDialog(null, file.getName() + " does not include JndiLookup.class.", "Warning", JOptionPane.WARNING_MESSAGE);
-                            successful = false;
-                        }
-                    }
+                    boolean successful = removeJndiLookupZipFS(file);
                     if (successful) {
                         JOptionPane.showMessageDialog(null, "Removed JndiLookup.class from " + file.getName(), "Success", JOptionPane.PLAIN_MESSAGE);
+                    } else {
+                        JOptionPane.showMessageDialog(null, file.getName() + " does not include JndiLookup.class.", "Warning", JOptionPane.WARNING_MESSAGE);
                     }
                 } catch (IOException e) {
                     JOptionPane.showMessageDialog(null, file.getName() + " could not be modified.", "Error", JOptionPane.ERROR_MESSAGE);
                 }
             }
+        }
+    }
+
+    private static boolean removeJndiLookupZipFS(File file) throws IOException {
+        boolean modified = false;
+        try (FileSystem fs = FileSystems.newFileSystem(file.toPath(), null)) {
+            PathMatcher jndiLookupMatcher = fs.getPathMatcher(JNDI_LOOKUP_PATTERN);
+            for (Path rootDir : fs.getRootDirectories()) {
+
+                List<Path> jndiPathList = Files.walk(rootDir)
+                        .filter(jndiLookupMatcher::matches)
+                        .collect(Collectors.toList());
+
+                if (!jndiPathList.isEmpty()) {
+                    modified = true;
+                    for (Path jndiPath : jndiPathList) {
+                        Files.delete(jndiPath);
+                    }
+                }
+            }
+            return modified;
         }
     }
 
